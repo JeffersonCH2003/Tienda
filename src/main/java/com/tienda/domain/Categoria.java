2b11fb0a0c5700e15a086a110533b71ce1113f29
AMD <AMD@DESKTOP-SKAI2UP>
2023-10-13 18:16:37 -0600

    Semana 5, Categorias, creación, etc..

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,34 @@
+
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data   //Esta clase va a tener datos
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column( name ="id_categoria")
+    private Long idCategoria;
+    
+    private String descripcion;
+    private String rutaImagen;
+    private Boolean activo;
+
+    public Categoria() {   
+    }
+
+    public Categoria(String descripcion, Boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+    
+   
+}
