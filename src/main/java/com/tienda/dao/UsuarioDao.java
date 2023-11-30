71379c382386b932ec57bcbea4112d361fb03056
AMD <AMD@DESKTOP-SKAI2UP>
2023-11-30 16:00:03 -0600

    Clase grabada del miercoles en la noche para estar al día con la, materia(motivos del profe), creacion de clases java, interfaces, usuarios, correo etc,por último agregar usuarios.jrxml y ventas.jrxml como reportes,y algunos comentarios de ayuda :) .

--- src/main/java/com/tienda/dao/UsuarioDao.java
+++ src/main/java/com/tienda/dao/UsuarioDao.java
@@ -6,6 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
     Usuario findByUsername(String username);
+    
+    //3 método agregados
+    Usuario findByUsernameAndPassword(String username, String Password);//buscar un usuario y contraseña
+
+    Usuario findByUsernameOrCorreo(String username, String correo);//busca usuario o correo
+
+    boolean existsByUsernameOrCorreo(String username, String correo);//funcion booleana, valida si existe el usuario o correo, trate de buscar si existe o no.
 }
 
     
